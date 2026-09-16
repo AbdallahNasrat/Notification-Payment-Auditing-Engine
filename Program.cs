4ff0ec057891bcd6380561c21ae043e14cdbf41b
Abdallah Nasrat <Abdonasrat1@gmail.com>
2026-09-16 15:09:26 +0300

    feat: Grouping Services in Extension Method in DependecyInjectionExtension Class

--- Program.cs
+++ Program.cs
@@ -6,16 +6,13 @@ namespace DotNet_DependencyInjection_Lab
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.AddTransient<IGuidService, TransientGuidService>(); // Different values if is at the last
-            builder.Services.AddSingleton<IGuidService, SingletonGuidService>(); //the same values if is at the last even when do new request
-            builder.Services.AddScoped<IGuidService, ScopedGuidService>();// the same values if is at the last , because one request , but changed after new request
+            //Grouping Services (Service Registration Extensions
+            builder.Services.AddDIServices();
 
 
 
 
-            builder.Services.AddScoped<AuditService>();
-
-
+            
             var app = builder.Build();
 
             app.MapGet("/testLifeTime", (AuditService auditService ) => {
